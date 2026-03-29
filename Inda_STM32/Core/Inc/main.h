fe67e552d9b4063d45e26c0377d7f0a461cdd34a
zzvladyzz <vladimir_1913@hotmail.com>
2026-03-29 08:51:49 +0200

    Primer commit

---
+++ Inda_STM32/Core/Inc/main.h
@@ -0,0 +1,109 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_OK_Pin GPIO_PIN_13
+#define LED_OK_GPIO_Port GPIOC
+#define LED_1_Pin GPIO_PIN_14
+#define LED_1_GPIO_Port GPIOC
+#define LED_2_Pin GPIO_PIN_15
+#define LED_2_GPIO_Port GPIOC
+#define AN0_Pin GPIO_PIN_0
+#define AN0_GPIO_Port GPIOA
+#define AN1_Pin GPIO_PIN_1
+#define AN1_GPIO_Port GPIOA
+#define AN2_Pin GPIO_PIN_2
+#define AN2_GPIO_Port GPIOA
+#define AN3_Pin GPIO_PIN_3
+#define AN3_GPIO_Port GPIOA
+#define Selector_Pin GPIO_PIN_4
+#define Selector_GPIO_Port GPIOA
+#define Current_ML_Pin GPIO_PIN_5
+#define Current_ML_GPIO_Port GPIOA
+#define Current_MR_Pin GPIO_PIN_6
+#define Current_MR_GPIO_Port GPIOA
+#define Voltage_Pin GPIO_PIN_7
+#define Voltage_GPIO_Port GPIOA
+#define Output_1_Pin GPIO_PIN_0
+#define Output_1_GPIO_Port GPIOB
+#define Output_2_Pin GPIO_PIN_1
+#define Output_2_GPIO_Port GPIOB
+#define Output_3_Pin GPIO_PIN_2
+#define Output_3_GPIO_Port GPIOB
+#define Output_4_Pin GPIO_PIN_12
+#define Output_4_GPIO_Port GPIOB
+#define EN_MOTOR_Pin GPIO_PIN_12
+#define EN_MOTOR_GPIO_Port GPIOA
+#define Right_Line_Pin GPIO_PIN_3
+#define Right_Line_GPIO_Port GPIOB
+#define Back_Line_Pin GPIO_PIN_4
+#define Back_Line_GPIO_Port GPIOB
+#define Left_Line_Pin GPIO_PIN_5
+#define Left_Line_GPIO_Port GPIOB
+#define LED_ALARMA_Pin GPIO_PIN_9
+#define LED_ALARMA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ README.md
@@ -0,0 +1,1 @@
+# Inda_Sumo
